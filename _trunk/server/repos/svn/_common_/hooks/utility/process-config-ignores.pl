4684029b105c2e672fd0516e9302acad66b0217d
Rob <Rob@ba238644-3430-7861-efe2-d5b3ebe22013>
2010-08-04 21:36:01 +0000

    added licence block to the larger scripts

--- _trunk/server/repos/svn/_common_/hooks/utility/process-config-ignores.pl
+++ _trunk/server/repos/svn/_common_/hooks/utility/process-config-ignores.pl
@@ -1,5 +1,36 @@
 #!/usr/bin/perl
 
+###############################################################################
+###
+### Copyright © 2010 Rob Hubbard.
+###
+### This file forms part of Rob Hubbard's Subversion Configuration Suite
+### (the "Software").
+###
+### Permission is hereby granted, free of charge, to any person obtaining a
+### copy of the Software and associated documentation files, to deal in the
+### Software without restriction, including without limitation the rights to
+### use, copy, modify, merge, publish, and/or distribute (free of charge)
+### copies of the Software, and to permit persons to whom the Software is
+### furnished to do so, subject to the following conditions:
+###
+### The above copyright notice and this permission notice shall be included in
+### all copies or substantial portions of the Software. For clarity, the above
+### copyright notice and this permission notice do not apply to any software
+### developed using but not incorporating the Software.
+###
+### The Software is provided "as is", without warranty of any kind, express or
+### implied, including but not limited to the warranties of merchantability,
+### fitness for a particular purpose and noninfringement. In no event shall the
+### authors or copyright holders be liable for any claim, damages or other
+### liability, whether in an action of contract, tort or otherwise, arising
+### from, out of or in connection with the Software or the use or other
+### dealings in the Software. 
+###
+### "Copyleft; All wrongs reversed."
+###
+###############################################################################
+
 $success = open ($fh, "<", "$ARGV[0]");
 if ($success)
 {

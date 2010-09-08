787e73b66d68953203a67b0b4c740308cba59601
Rob <Rob@ba238644-3430-7861-efe2-d5b3ebe22013>
2010-09-08 23:37:03 +0000

    demoted licence of lesser scripts

--- _trunk/server/repos/svn/sandpit/hooks/_template/_hook_template_.bat
+++ _trunk/server/repos/svn/sandpit/hooks/_template/_hook_template_.bat
@@ -8,32 +8,9 @@ REM
 REM $URL$
 REM $Rev$
 REM
-REM Copyright © 2010 Rob Hubbard.
+REM This file accompanies Rob Hubbard's Subversion Configuration Suite
 REM
-REM This file forms part of Rob Hubbard's Subversion Configuration Suite
-REM (the "Software").
-REM
-REM Permission is hereby granted, free of charge, to any person obtaining a
-REM copy of the Software and associated documentation files, to deal in the
-REM Software without restriction, including without limitation the rights to
-REM use, copy, modify, merge, publish, and/or distribute (free of charge)
-REM copies of the Software, and to permit persons to whom the Software is
-REM furnished to do so, subject to the following conditions:
-REM
-REM The above copyright notice and this permission notice shall be included in
-REM all copies or substantial portions of the Software. For clarity, the above
-REM copyright notice and this permission notice do not apply to any software
-REM developed using but not incorporating the Software.
-REM
-REM The Software is provided "as is", without warranty of any kind, express or
-REM implied, including but not limited to the warranties of merchantability,
-REM fitness for a particular purpose and noninfringement. In no event shall the
-REM authors or copyright holders be liable for any claim, damages or other
-REM liability, whether in an action of contract, tort or otherwise, arising
-REM from, out of or in connection with the Software or the use or other
-REM dealings in the Software. 
-REM
-REM "Copyleft; All wrongs reversed."
+REM @ 2010, Rob Hubbard.
 REM
 REM ###########################################################################
 

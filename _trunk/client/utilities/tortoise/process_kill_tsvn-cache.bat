cca08dac4774647ed39cbd6ee190f36c422eeaf6
Rob <Rob@ba238644-3430-7861-efe2-d5b3ebe22013>
2010-09-08 23:43:59 +0000

    added minor headers to tsvn process scripts

--- _trunk/client/utilities/tortoise/process_kill_tsvn-cache.bat
+++ _trunk/client/utilities/tortoise/process_kill_tsvn-cache.bat
@@ -1,4 +1,18 @@
 @echo off
 
+REM ###########################################################################
+REM
+REM TortoiseSVN Cache Resetter
+REM --------------------------
+REM
+REM $URL$
+REM $Rev$
+REM
+REM This file accompanies Rob Hubbard's Subversion Configuration Suite
+REM
+REM @ 2010, Rob Hubbard.
+REM
+REM ###########################################################################
+
 echo.
 taskkill /IM "TSVNCache.exe" /F /T
--- _trunk/client/utilities/tortoise/process_list_tsvn.bat
+++ _trunk/client/utilities/tortoise/process_list_tsvn.bat
@@ -1,4 +1,18 @@
 @echo off
 
+REM ###########################################################################
+REM
+REM TortoiseSVN Process Lister
+REM --------------------------
+REM
+REM $URL$
+REM $Rev$
+REM
+REM This file accompanies Rob Hubbard's Subversion Configuration Suite
+REM
+REM @ 2010, Rob Hubbard.
+REM
+REM ###########################################################################
+
 echo.
 tasklist | findstr "Tortoise TSVN"
